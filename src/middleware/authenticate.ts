4ecf059fb2afb17408d64af7550f00822e597a8b
SatoKyohei <76243374+SatoKyohei@users.noreply.github.com>
2024-12-29 15:50:23 +0900

    Update authenticate.ts

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -32,6 +32,7 @@ export const authenticateToken = (
 
     // 署名の検証に成功：errにnullが格納され、userにペイロードが格納される
     // 署名の検証に失敗：errにエラー情報が格納され、userはundefined。
+    // jwt.verify：1.このトークンを解読。2. トークンからペイロード部分（{ userId: 123 }）を取り出す。3. このデータを user パラメータ（3つ目の引数）としてコールバック関数に渡す
     jwt.verify(token, SECRET_KEY, (err, user) => {
         if (err) {
             res.status(403).json({ error: "Invalid token" });
